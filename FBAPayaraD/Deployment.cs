4acc562627d73326846418b4150ca7bed3c4b008
Will Taylor <william.h.taylor@stfc.ac.uk>
2022-05-16 17:20:40 +0100

    Show git commit in list

--- FBAPayaraD/Deployment.cs
+++ FBAPayaraD/Deployment.cs
@@ -10,28 +10,40 @@ namespace FBAPayaraD
         private readonly string _version;
         private readonly DateTime? _deployTime;
         private readonly string _repoBranch;
+        private readonly string _repoCommit;
 
         public Deployment(
             Service service,
             string version,
             DateTime? deployTime,
-            string repoBranch = null)
+            string repoBranch = null,
+            string repoCommit = null)
         {
             Service = service;
             _version = version;
             _deployTime = deployTime;
             _repoBranch = repoBranch;
+            _repoCommit = repoCommit;
         }
 
-        public string Serialize() => string.Join(",", Values());
+        public string Serialize() =>
+            $"{Service},{_version}, {_deployTime:u},{_repoBranch},{_repoCommit}";
 
-        public List<string> Values() => new()
+        public List<string> Values()
         {
-            Service.ToString(),
-            _version,
-            _deployTime?.ToString("u") ?? "",
-            _repoBranch ?? "",
-        };
+            var repoValue = _repoBranch ?? "";
+            repoValue += string.IsNullOrEmpty(_repoCommit)
+                ? ""
+                : $"({_repoCommit})";
+
+            return new()
+            {
+                Service.ToString(),
+                _version,
+                _deployTime?.ToString("u") ?? "",
+                repoValue,
+            };
+        }
 
         public static Deployment Deserialize(string serialized)
         {
@@ -50,6 +62,7 @@ namespace FBAPayaraD
         private string _version;
         private DateTime? _deployTime;
         private string _repoBranch;
+        private string _repoCommit;
 
         public DeploymentBuilder FromWar(string war)
         {
@@ -67,13 +80,14 @@ namespace FBAPayaraD
         public DeploymentBuilder ForRepo(Git repo)
         {
             _repoBranch = repo.Branch();
+            _repoCommit = repo.HeadRef();
             return this;
         }
 
         public Deployment Build()
         {
             return new Deployment(
-                _service, _version, _deployTime, _repoBranch
+                _service, _version, _deployTime, _repoBranch, _repoCommit
             );
         }
     }
